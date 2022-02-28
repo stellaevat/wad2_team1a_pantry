61493415f7e4e6bb55905407de0a577f93bc0cd6
Calum McGregor <2518508M@student.gla.ac.uk>
2022-02-28 16:51:02 +0000

    Started Base Fields of Models, got to start adding relationships between models

--- pantry/admin.py
+++ pantry/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from pantry.models import UserProfile
+from pantry.models import UserProfile, User, Category, Ingredient, Recipe
 
 admin.site.register(UserProfile)
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
--- pantry/models.py
+++ pantry/models.py
@@ -8,3 +8,62 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Recipe(models.Model):
+    Title = models.CharField(max_length=128)
+    Steps = models.CharField(max_length=2048)
+    PrepTime = models.IntegerField()
+    CookTime = models.IntegerField()
+    Servings = models.IntegerField()
+    Difficulty = models.CharField(
+        max_length = 16,
+        choices = (
+            ("easy", "Easy"),
+            ("medium", "Medium"),
+            ("hard", "Hard")
+        ) 
+    )
+    Picture = models.ImageField(upload_to='recipe_pictures', blank=True)
+    PubDate = models.DateField()
+    Start = models.IntegerField()
+
+    def __str__(self):
+        return self.Title
+
+class User(models.Model):
+    Email = models.CharField(max_length=254)
+    Username = models.CharField(max_length=128)
+    Password = models.CharField(max_length=128)
+    ProfilePicture = models.ImageField(upload_to='profile_pictures', blank=True)
+
+    def __str__(self):
+        return self.Username
+
+class Category(models.Model):
+    Name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.Name
+
+class Ingredient(models.Model):
+    Name = models.CharField(max_length=128)
+    Type = models.CharField(
+        max_length = 128,
+        choices=(
+            ("meat", "Meat, seafood & substitutes"),
+            ("eggs", "Eggs, dairy & substitutes"),
+            ("veg", "Vegetables & Funghi"),
+            ("pulses", "Pulses"),
+            ("grains", "Grains, seeds & nuts"),
+            ("bread", "Bread, pasta & rice"),
+            ("fats", "Fats & Oils"),
+            ("herbs", "Herbs & Spices"),
+            ("condiments", "Condiments & Sauces"),
+            ("fruit", "Fruit"),
+            ("sweets", "Sweet & Baking"),
+            ("beverages", "Beverages")
+        )
+    )
+
+    def __str__(self):
+        return self.Name
