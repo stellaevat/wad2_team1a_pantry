061d17aa32388ce541fb351a23212137cb014feb
Lara D'Agata <2526633d@student.gla.ac.uk>
2022-03-07 19:52:58 +0000

    Added Unit Testing for Template Structure

--- pantry/tests.py
+++ pantry/tests.py
@@ -53,3 +53,64 @@ class PantryProjectStructureTests(TestCase):
         is_app_configured = 'pantry' in settings.INSTALLED_APPS
 
         self.assertTrue(is_app_configured, f"{FAILURE_HEADER}The pantry app is missing from your setting's INSTALLED_APPS list.{FAILURE_FOOTER}")
+
+"""
+Tests to check the initial structure of templates
+"""
+class PantryTemplateStructureTests(TestCase):
+
+    def setUp(self):
+        self.project_base_dir = os.getcwd()
+        self.templates_dir = os.path.join(self.project_base_dir, 'templates')
+        self.pantry_templates_dir = os.path.join(self.templates_dir, 'pantry')
+
+    def test_templates_directory_exists(self):
+        """
+        Does the templates/ directory exist?
+        """
+        directory_exists = os.path.isdir(self.templates_dir)
+        self.assertTrue(directory_exists, f"{FAILURE_HEADER}Your project's templates directory does not exist.{FAILURE_FOOTER}")
+
+    def test_pantry_templates_directory_exists(self):
+        """
+        Does the templates/pantry/ directory exist?
+        """
+        directory_exists = os.path.isdir(self.pantry_templates_dir)
+        self.assertTrue(directory_exists, f"{FAILURE_HEADER}The Pantry templates directory does not exist.{FAILURE_FOOTER}")
+
+    def test_template_dir_setting(self):
+        """
+        Does the TEMPLATE_DIR setting exist, and does it point to the right directory?
+        """
+        variable_exists = 'TEMPLATE_DIR' in dir(settings)
+        self.assertTrue(variable_exists, f"{FAILURE_HEADER}Your settings.py module does not have the variable TEMPLATE_DIR defined!{FAILURE_FOOTER}")
+
+        template_dir_value = os.path.normpath(settings.TEMPLATE_DIR)
+        template_dir_computed = os.path.normpath(self.templates_dir)
+        self.assertEqual(template_dir_value, template_dir_computed, f"{FAILURE_HEADER}Your TEMPLATE_DIR setting does not point to the expected path. Check your configuration, and try again.{FAILURE_FOOTER}")
+
+    def test_template_lookup_path(self):
+        """
+        Does the TEMPLATE_DIR value appear within the lookup paths for templates?
+        """
+        lookup_list = settings.TEMPLATES[0]['DIRS']
+        found_path = False
+
+        for entry in lookup_list:
+            entry_normalised = os.path.normpath(entry)
+
+            if entry_normalised == os.path.normpath(settings.TEMPLATE_DIR):
+                found_path = True
+
+        self.assertTrue(found_path, f"{FAILURE_HEADER}Your project's templates directory is not listed in the TEMPLATES>DIRS lookup list. Check your settings.py module.{FAILURE_FOOTER}")
+
+    def test_templates_exist(self):
+        """
+        Do the base.html and home.html templates exist in the correct place?
+        (Maybe add more checks for all html files in templates directory)
+        """
+        base_path = os.path.join(self.pantry_templates_dir, 'base.html')
+        home_path = os.path.join(self.pantry_templates_dir, 'home.html')
+
+        self.assertTrue(os.path.isfile(base_path), f"{FAILURE_HEADER}Your base.html template does not exist, or is in the wrong location.{FAILURE_FOOTER}")
+        self.assertTrue(os.path.isfile(home_path), f"{FAILURE_HEADER}Your home.html template does not exist, or is in the wrong location.{FAILURE_FOOTER}")
