681cf1a44e2909f85e3c00693047c32e741a725f
Stella Eva Tsiapali <2526934t@student.gla.ac.uk>
2022-03-08 15:51:31 +0000

    Small grid fixed, sorter moved to separate html

--- pantry/templatetags/pantry_template_tags.py
+++ pantry/templatetags/pantry_template_tags.py
@@ -21,6 +21,11 @@ def recipe_display_grid(recipe, small=False, by_ingredient=False, selected=None)
             'by_ingredient': by_ingredient,
             'selected': selected,
             'ingredients': ingredients,}
+ 
+@register.inclusion_tag('pantry/recipe_sorter.html') 
+def get_recipe_sorter(category=None):
+    return {'category': category}
+    
     
 @register.inclusion_tag('pantry/ingredient_selection.html')
 def get_ingredient_selection(types, ingredients):
