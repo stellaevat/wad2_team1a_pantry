b5e281af40919ac44f6d93015d0a8a264c0d4afc
Stella Eva Tsiapali <2526934t@student.gla.ac.uk>
2022-03-08 19:55:09 +0000

    My recipes/Starred recipes pages made, including sorting function

--- pantry/urls.py
+++ pantry/urls.py
@@ -16,7 +16,9 @@ urlpatterns = [
     path('user/<str:username>/', views.user_profile, name="user_profile"),
     path('user/<str:username>/edit_profile/', views.edit_profile, name="edit_profile"),
     path('user/<str:username>/my_recipes/', views.show_my_recipes, name="my_recipes"),
+    path('user/<str:username>/my_recipes/<slug:sort>', views.show_my_recipes, name="my_recipes"),
     path('user/<str:username>/starred_recipes/', views.show_starred_recipes, name="starred_recipes"),
+    path('user/<str:username>/starred_recipes/<slug:sort>', views.show_starred_recipes, name="starred_recipes"),
     
     path('recipe/<slug:recipe_name_slug>/', views.show_recipe, name="show_recipe"),
     path('category/<slug:category_title_slug>/', views.show_category, name="show_category"),
