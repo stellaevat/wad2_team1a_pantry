656914283acbcfe9833873f427ff64f09f0408d2
Calum McGregor <2518508M@student.gla.ac.uk>
2022-03-14 10:49:07 +0000

    2 New Recipes added

---
+++ Irish Stew.md
@@ -0,0 +1,38 @@
+Irish Stew
+Heat the slow cooker if necessary, then heat the oil in a frying pan. Sizzle the bacon until crisp, tip into the slow-cooker pot, then brown the chunks of lamb in the pan. Transfer to the slow-cooker pot along with the thyme, onions, carrots, potatoes, stock, bay leaves and enough water to cover the lamb. Cover and cook on Low for 7 hrs.
+Stir in the pearl barley and leek, and cook on High for 1 hr more until the pearl barley is tender.
+Stir in the butter, season and serve scooped straight from the dish.
+
+Sunflower Oil 1 Tbsp
+Bacon 200g
+Lamb 900g
+Thyme Small Bunch
+Onions 3
+Carrots 5
+Potatoes 6
+Lamb Stock 700ml
+Bay Leaf 3
+Barley 85g
+Leek 1
+Butter Small Knob
+
+Garlic Chicken Parcels
+Heat oven to 220C/200C Fan/gas 7. Cut a slit halfway in each chicken breast, then put each one between two pieces of baking parchment and bash with a rolling pin to flatten slightly./n
+Cut your sheet of pastry in half widthways and put both halves on a baking tray. Sit a chicken breast on top of each half, then cram the cavities with the Boursin (don’t worry if a bit of cheese oozes out). Season, scatter over the lemon zest then fold the edges of the pastry in to the centre and pinch shut. Flip each parcel over so the seal is on the bottom. Brush with a little oil, then bake for 30 mins until deep golden./n
+About 10 mins before your parcels are ready, steam or boil the greens until tender. Toss in the oil and season. Serve alongside your chicken parcels with lemon wedges for squeezing over.
+
+Chicken 2 Breasts of
+Puff Pastry 320g
+Garlic 150g
+Lemon 1/2 of a
+Olive Oil 1 tbsp
+Green Beans 200g
+
+10 min prep
+20 min cook
+serves 2 
+easy
+Easy Meals, Mains, Under 30'
+
+
+
--- population_script.py
+++ population_script.py
@@ -38,54 +38,85 @@ def populate():
     ingredients = [
         {"name" : "Cheese", "ingredient_type" : "dairy"},
         {"name" : "Butter", "ingredient_type" : "dairy"},
-        {"name" : "Tomatoes", "ingredient_type" : "fruit"},
+        {"name" : "Tomato", "ingredient_type" : "fruit"},
         {"name" : "Beef", "ingredient_type" : "meats"},
         {"name" : "Ice Cream", "ingredient_type" : "sweets"},
-        {"name" : "Biscuits", "ingredient_type" : "sweets"},
+        {"name" : "Biscuit", "ingredient_type" : "sweets"},
         {"name" : "Bread", "ingredient_type" : "carbs"},
         {"name" : "Rice", "ingredient_type" : "carbs"},
         {"name" : "Lettuce", "ingredient_type" : "veg"},
         {"name" : "Water", "ingredient_type" : "drinks"},
-        {"name" : "Noodles", "ingredient_type" : "carbs"},
+        {"name" : "Noodle", "ingredient_type" : "carbs"},
         {"name" : "Spaghetti", "ingredient_type" : "carbs"},
         {"name" : "Flour", "ingredient_type" : "carbs"},
-        {"name" : "Eggs", "ingredient_type" : "dairy"},
+        {"name" : "Egg", "ingredient_type" : "dairy"},
         {"name" : "Milk", "ingredient_type" : "dairy"},
         {"name" : "Sunflower Oil", "ingredient_type" : "fats"},
         {"name" : "Salt", "ingredient_type" : "condiments"},
-        {"name" : "Baby Potatoes", "ingredient_type" : "carbs"},
+        {"name" : "Baby Potato", "ingredient_type" : "carbs"},
         {"name" : "Olive Oil", "ingredient_type" : "fats"},
-        {"name" : "Red Onions", "ingredient_type" : "veg"},
-        {"name" : "Red Peppers", "ingredient_type" : "veg"},
+        {"name" : "Red Onion", "ingredient_type" : "veg"},
+        {"name" : "Red Pepper", "ingredient_type" : "veg"},
         {"name" : "Garlic", "ingredient_type" : "veg"},
-        {"name" : "Sugar", "ingredient_type" : "sweets"}
-
+        {"name" : "Sugar", "ingredient_type" : "sweets"},
+        {"name" : "Bacon", "ingredient_type" : "meats"},
+        {"name" : "Lamb", "ingredient_type" : "meats"},
+        {"name" : "Thyme", "ingredient_type" : "herbs"},
+        {"name" : "Onion", "ingredient_type" : "veg"},
+        {"name" : "Carrot", "ingredient_type" : "veg"},
+        {"name" : "Potato", "ingredient_type" : "veg"},
+        {"name" : "Lamb Stock", "ingredient_type" : "condiments"},
+        {"name" : "Bay Leaf", "ingredient_type" : "herbs"},
+        {"name" : "Barley", "ingredient_type" : "herbs"},
+        {"name" : "Leek", "ingredient_type" : "veg"},
+        {"name" : "Puff Pastry", "ingredient_type" : "carbs"},
+        {"name" : "Chicken", "ingredient_type" : "meats"},
+        {"name" : "Lemon", "ingredient_type" : "fruit"},
+        {"name" : "Green Bean", "ingredient_type" : "veg"},
     ]
 
     
     recipes = [
         {"name":"Halloumi traybake", "author" : "johndoe", "steps" : 
         """
-Heat oven to 160C/140C fan/gas 3. Put the potatoes in a large roasting tin with the onion. Pour over 2 tbsp olive oil and roast in the oven for about 30 mins.
-Add the chickpeas, pepper, romanesco, tomatoes and garlic. Drizzle with 2 tbsp oil, then roast for a further 20-25 mins until everything is cooked and browning nicely. Toss together briefly and put the halloumi slices on top. Put it under the grill for 5-10 mins, or until the cheese is melting and browning (keep an eye on it). Scatter over the basil leaves to serve.
-        """, "ingredients" : {"Baby Potatoes":"2kg", "Olive Oil":"2 tbsp", "Red Onions":"1", "Red Peppers":"2", "Garlic":"3 cloves"}, "category" : ["Vegan", "Healthy", "Vegetarian"], "prep_time" : 15, "cook_time" : 60, "servings" : 4,
+Heat oven to 160C/140C fan/gas 3. /n Put the potatoes in a large roasting tin with the onion. /n Pour over 2 tbsp olive oil and roast in the oven for about 30 mins. /n
+Add the chickpeas, pepper, romanesco, tomatoes and garlic. /n Drizzle with 2 tbsp oil, then roast for a further 20-25 mins until everything is cooked and browning nicely./n Toss together briefly and put the halloumi slices on top./n Put it under the grill for 5-10 mins, or until the cheese is melting and browning (keep an eye on it)./n Scatter over the basil leaves to serve.
+        """, "ingredients" : {"Baby Potato":"2kg", "Olive Oil":"2 tbsp", "Red Onion":"1", "Red Pepper":"2", "Garlic":"3 cloves"}, "category" : ["Vegan", "Healthy", "Vegetarian"], "prep_time" : 15, "cook_time" : 60, "servings" : 4,
          "difficulty" : "medium", "pub_date" : datetime.date(2022,2,27), "stars" : 32, "filename" : "halloumi-traybake.jpg"},
 
         {"name":"Pancakes", "author" : "sallywalker", "steps" : 
         """
-Put 100g plain flour and a pinch of salt into a large mixing bowl. Make a well in the centre and crack 2 eggs into the middle.
-Pour in about 50ml from the 300ml of semi-skimmed milk and 1 tbsp sunflower oil then start whisking from the centre, gradually drawing the flour into the eggs, milk and oil. Once all the flour is incorporated, beat until you have a smooth, thick paste. Add a little more milk if it is too stiff to beat.
-Add a good splash of milk and whisk to loosen the thick batter. While still whisking, pour in a steady stream of the remaining milk. Continue pouring and whisking until you have a batter that is the consistency of slightly thick single cream.
+Put 100g plain flour and a pinch of salt into a large mixing bowl./n Make a well in the centre and crack 2 eggs into the middle./n
+Pour in about 50ml from the 300ml of semi-skimmed milk and 1 tbsp sunflower oil then start whisking from the centre, gradually drawing the flour into the eggs, milk and oil./n Once all the flour is incorporated, beat until you have a smooth, thick paste./n Add a little more milk if it is too stiff to beat./n
+Add a good splash of milk and whisk to loosen the thick batter./n While still whisking, pour in a steady stream of the remaining milk./n Continue pouring and whisking until you have a batter that is the consistency of slightly thick single cream./n
 Heat the pan over a moderate heat, then wipe it with oiled kitchen paper.
-        """, "ingredients" : {"Flour":"100g", "Eggs":"2", "Milk":"300ml", "Sunflower Oil":"1 tbsp", "Salt":"a pinch"}, "category" : ["Vegetarian", "Under 30'","Desserts", "Easy Meals"], "prep_time" : 10, "cook_time" : 30, "servings" : 4,
+        """, "ingredients" : {"Flour":"100g", "Egg":"2", "Milk":"300ml", "Sunflower Oil":"1 tbsp", "Salt":"a pinch"}, "category" : ["Vegetarian", "Under 30'","Desserts", "Easy Meals"], "prep_time" : 10, "cook_time" : 30, "servings" : 4,
          "difficulty" : "easy", "pub_date" : datetime.date(2022,2,28), "stars" : 64, "filename" : "pancakes.jpg"},
 
          {"name":"New York cheesecake", "author" : "benking", "steps" : 
         """
-Position an oven shelf in the middle of the oven. Heat the oven to 180C/ 160C fan/ gas 4.
-Line the base of a 23cm springform cake tin by putting a square piece of parchment paper or foil on top of the tin base and then clipping the side on so the paper or foil is trapped and any excess sticks out of the bottom.
-        """, "ingredients" : {"Butter":"2 tbsp", "Biscuits":"50g", "Sugar":"1/2 cup", "Cheese":"500g", "Flour":"50g"}, "category" : ["Vegetarian", "Desserts"], "prep_time" : 20, "cook_time" : 70, "servings" : 12,
-         "difficulty" : "hard", "pub_date" : datetime.date(2022,1,13), "stars" : 112, "filename" : "new-york-cheesecake.jpg"}
+Position an oven shelf in the middle of the oven. Heat the oven to 180C/ 160C fan/ gas 4./n
+Line the base of a 23cm springform cake tin by putting a square piece of parchment paper or foil on top of the tin base and then clipping the side on so the paper or foil is trapped and any excess sticks out of the bottom./n
+        """, "ingredients" : {"Butter":"2 tbsp", "Biscuit":"50g", "Sugar":"1/2 cup", "Cheese":"500g", "Flour":"50g"}, "category" : ["Vegetarian", "Desserts"], "prep_time" : 20, "cook_time" : 70, "servings" : 12,
+         "difficulty" : "hard", "pub_date" : datetime.date(2022,1,13), "stars" : 112, "filename" : "new-york-cheesecake.jpg"},
+
+         {"name":"Irish Stew", "author" : "joebloggs", "steps" : 
+        """
+Heat the slow cooker if necessary, then heat the oil in a frying pan. Sizzle the bacon until crisp, tip into the slow-cooker pot, then brown the chunks of lamb in the pan. Transfer to the slow-cooker pot along with the thyme, onions, carrots, potatoes, stock, bay leaves and enough water to cover the lamb. Cover and cook on Low for 7 hrs./n
+Stir in the pearl barley and leek, and cook on High for 1 hr more until the pearl barley is tender./n
+Stir in the butter, season and serve scooped straight from the dish.
+        """, "ingredients" : {"Butter":"A small knob of", "Sunflower Oil":"1 Tbsp", "Bacon":"200g", "Lamb":"900g", "Thyme":"A small bunch of", "Onion" : "3", "Carrot" : "5", "Potato" : "6", "Lamb Stock" : "700ml", "Bay Leaf" : "3", "Barley" : "85g", "Leek" : "1"}, "category" : ["Mains", "Easy Meals"], "prep_time" : 20, "cook_time" : 480, "servings" : 6,
+         "difficulty" : "hard", "pub_date" : datetime.date(2021,12,25), "stars" : 5, "filename" : "irish-stew.jpg"},
+
+         {"name":"Garlic Chicken Parcels", "author" : "joebloggs", "steps" : 
+        """
+Heat oven to 220C/200C Fan/gas 7. Cut a slit halfway in each chicken breast, then put each one between two pieces of baking parchment and bash with a rolling pin to flatten slightly./n
+Cut your sheet of pastry in half widthways and put both halves on a baking tray. Sit a chicken breast on top of each half, then cram the cavities with the Boursin (don’t worry if a bit of cheese oozes out). Season, scatter over the lemon zest then fold the edges of the pastry in to the centre and pinch shut. Flip each parcel over so the seal is on the bottom. Brush with a little oil, then bake for 30 mins until deep golden./n
+About 10 mins before your parcels are ready, steam or boil the greens until tender. Toss in the oil and season. Serve alongside your chicken parcels with lemon wedges for squeezing over.
+        """, "ingredients" : {"Chicken":"2 Breasts of", "Olive Oil":"1 tbsp", "Green Bean":"200g", "Garlic":"150g", "Puff Pastry":"320g", "Lemon" : "1/2 of a"}, "category" : ["Mains", "Easy Meals", "Under 30'"], "prep_time" : 10, "cook_time" : 20, "servings" : 2,
+         "difficulty" : "easy", "pub_date" : datetime.date(2021,3,20), "stars" : 60, "filename" : "garlic-chicken-parcels.jpg"}
+
+         
     ]
 
 
