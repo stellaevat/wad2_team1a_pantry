a034baeffa324dc7371408ab8de434bce33f8be6
Calum McGregor <2518508M@student.gla.ac.uk>
2022-03-14 16:11:16 +0000

    Added more recipes

--- population_script.py
+++ population_script.py
@@ -73,50 +73,73 @@ def populate():
         {"name" : "Chicken", "ingredient_type" : "meats"},
         {"name" : "Lemon", "ingredient_type" : "fruit"},
         {"name" : "Green Bean", "ingredient_type" : "veg"},
+        {"name" : "Pasta", "ingredient_type" : "carbs"},
+        {"name" : "Cream", "ingredient_type" : "dairy"},
+        {"name" : "Arugula", "ingredient_type" : "veg"},
+        {"name" : "Cranberry", "ingredient_type" : "fruit"},
+        {"name" : "Avocado", "ingredient_type" : "veg"},
+        {"name" : "Goat Cheese", "ingredient_type" : "dairy"},
+        {"name" : "Walnut", "ingredient_type" : "grains"},
+        {"name" : "Honey Mustard Vinaigrette", "ingredient_type" : "condiments"},
     ]
 
     
     recipes = [
         {"name":"Halloumi traybake", "author" : "johndoe", "steps" : 
-        """
-Heat oven to 160C/140C fan/gas 3. /n Put the potatoes in a large roasting tin with the onion. /n Pour over 2 tbsp olive oil and roast in the oven for about 30 mins. /n
-Add the chickpeas, pepper, romanesco, tomatoes and garlic. /n Drizzle with 2 tbsp oil, then roast for a further 20-25 mins until everything is cooked and browning nicely./n Toss together briefly and put the halloumi slices on top./n Put it under the grill for 5-10 mins, or until the cheese is melting and browning (keep an eye on it)./n Scatter over the basil leaves to serve.
-        """, "ingredients" : {"Baby Potato":["2kg",True], "Olive Oil":["2 tbsp",False], "Red Onion":["1",False], "Red Pepper":["2",True], "Garlic":["3 cloves",False]}, "category" : ["Vegan", "Healthy", "Vegetarian"], "prep_time" : 15, "cook_time" : 60, "servings" : 4,
+        """Heat oven to 160C/140C fan/gas 3.
+Put the potatoes in a large roasting tin with the onion.
+Pour over 2 tbsp olive oil and roast in the oven for about 30 mins.
+Add the chickpeas, pepper, romanesco, tomatoes and garlic.
+Drizzle with 2 tbsp oil, then roast for a further 20-25 mins until everything is cooked and browning nicely.
+Toss together briefly and put the halloumi slices on top.
+Put it under the grill for 5-10 mins, or until the cheese is melting and browning (keep an eye on it).
+Scatter over the basil leaves to serve.""", "ingredients" : {"Baby Potato":["2kg",True], "Olive Oil":["2 tbsp",False], "Red Onion":["1",False], "Red Pepper":["2",True], "Garlic":["3 cloves",False]}, "category" : ["Vegan", "Healthy", "Vegetarian"], "prep_time" : 15, "cook_time" : 60, "servings" : 4,
          "difficulty" : "medium", "pub_date" : datetime.date(2022,2,27), "stars" : 32, "filename" : "halloumi-traybake.jpg"},
 
         {"name":"Pancakes", "author" : "sallywalker", "steps" : 
-        """
-Put 100g plain flour and a pinch of salt into a large mixing bowl./n Make a well in the centre and crack 2 eggs into the middle./n
-Pour in about 50ml from the 300ml of semi-skimmed milk and 1 tbsp sunflower oil then start whisking from the centre, gradually drawing the flour into the eggs, milk and oil./n Once all the flour is incorporated, beat until you have a smooth, thick paste./n Add a little more milk if it is too stiff to beat./n
-Add a good splash of milk and whisk to loosen the thick batter./n While still whisking, pour in a steady stream of the remaining milk./n Continue pouring and whisking until you have a batter that is the consistency of slightly thick single cream./n
-Heat the pan over a moderate heat, then wipe it with oiled kitchen paper.
-        """, "ingredients" : {"Flour":["100g",False], "Egg":["2",True], "Milk":["300ml",False], "Sunflower Oil":["1 tbsp",False], "Salt":["a pinch",False]}, "category" : ["Vegetarian", "Under 30'","Desserts", "Easy Meals"], "prep_time" : 10, "cook_time" : 30, "servings" : 4,
+        """Put 100g plain flour and a pinch of salt into a large mixing bowl. Make a well in the centre and crack 2 eggs into the middle.
+Pour in about 50ml from the 300ml of semi-skimmed milk and 1 tbsp sunflower oil then start whisking from the centre, gradually drawing the flour into the eggs, milk and oil.
+Once all the flour is incorporated, beat until you have a smooth, thick paste. Add a little more milk if it is too stiff to beat.
+Add a good splash of milk and whisk to loosen the thick batter. While still whisking, pour in a steady stream of the remaining milk.Continue pouring and whisking until you have a batter that is the consistency of slightly thick single cream.
+Heat the pan over a moderate heat, then wipe it with oiled kitchen paper.""", "ingredients" : {"Flour":["100g",False], "Egg":["2",True], "Milk":["300ml",False], "Sunflower Oil":["1 tbsp",False], "Salt":["a pinch",False]}, "category" : ["Vegetarian", "Under 30'","Desserts", "Easy Meals"], "prep_time" : 10, "cook_time" : 30, "servings" : 4,
          "difficulty" : "easy", "pub_date" : datetime.date(2022,2,28), "stars" : 64, "filename" : "pancakes.jpg"},
 
          {"name":"New York cheesecake", "author" : "benking", "steps" : 
-        """
-Position an oven shelf in the middle of the oven. Heat the oven to 180C/ 160C fan/ gas 4./n
-Line the base of a 23cm springform cake tin by putting a square piece of parchment paper or foil on top of the tin base and then clipping the side on so the paper or foil is trapped and any excess sticks out of the bottom./n
-        """, "ingredients" : {"Butter":["2 tbsp",False], "Biscuit":["50g",True], "Sugar":["1/2 cup",False], "Cheese":["500g",False], "Flour":["50g",False]}, "category" : ["Vegetarian", "Desserts"], "prep_time" : 20, "cook_time" : 70, "servings" : 12,
+        """Position an oven shelf in the middle of the oven. Heat the oven to 180C/ 160C fan/ gas 4.
+Line the base of a 23cm springform cake tin by putting a square piece of parchment paper or foil on top of the tin base and then clipping the side on so the paper or foil is trapped and any excess sticks out of the bottom.""", "ingredients" : {"Butter":["2 tbsp",False], "Biscuit":["50g",True], "Sugar":["1/2 cup",False], "Cheese":["500g",False], "Flour":["50g",False]}, "category" : ["Vegetarian", "Desserts"], "prep_time" : 20, "cook_time" : 70, "servings" : 12,
          "difficulty" : "hard", "pub_date" : datetime.date(2022,1,13), "stars" : 112, "filename" : "new-york-cheesecake.jpg"},
 
          {"name":"Irish Stew", "author" : "joebloggs", "steps" : 
-        """
-Heat the slow cooker if necessary, then heat the oil in a frying pan. Sizzle the bacon until crisp, tip into the slow-cooker pot, then brown the chunks of lamb in the pan. Transfer to the slow-cooker pot along with the thyme, onions, carrots, potatoes, stock, bay leaves and enough water to cover the lamb. Cover and cook on Low for 7 hrs./n
-Stir in the pearl barley and leek, and cook on High for 1 hr more until the pearl barley is tender./n
-Stir in the butter, season and serve scooped straight from the dish.
-        """, "ingredients" : {"Butter":["A small knob of",False], "Sunflower Oil":["1 Tbsp",False], "Bacon":["200g",False], "Lamb":["900g",False], "Thyme":["A small bunch of",False], "Onion" : ["3",True], "Carrot" : ["5",True], "Potato" : ["6",True], "Lamb Stock" : ["700ml",False], "Bay Leaf" : ["3",True], "Barley" : ["85g",False], "Leek" : ["1",False]}, "category" : ["Mains", "Easy Meals"], "prep_time" : 20, "cook_time" : 480, "servings" : 6,
+        """Heat the slow cooker if necessary, then heat the oil in a frying pan. Sizzle the bacon until crisp, tip into the slow-cooker pot, then brown the chunks of lamb in the pan. Transfer to the slow-cooker pot along with the thyme, onions, carrots, potatoes, stock, bay leaves and enough water to cover the lamb. Cover and cook on Low for 7 hrs.
+Stir in the pearl barley and leek, and cook on High for 1 hr more until the pearl barley is tender.
+Stir in the butter, season and serve scooped straight from the dish.""", "ingredients" : {"Butter":["A small knob of",False], "Sunflower Oil":["1 Tbsp",False], "Bacon":["200g",False], "Lamb":["900g",False], "Thyme":["A small bunch of",False], "Onion" : ["3",True], "Carrot" : ["5",True], "Potato" : ["6",True], "Lamb Stock" : ["700ml",False], "Bay Leaf" : ["3",True], "Barley" : ["85g",False], "Leek" : ["1",False]}, "category" : ["Mains", "Easy Meals"], "prep_time" : 20, "cook_time" : 480, "servings" : 6,
          "difficulty" : "hard", "pub_date" : datetime.date(2021,12,25), "stars" : 5, "filename" : "irish-stew.jpg"},
 
          {"name":"Garlic Chicken Parcels", "author" : "joebloggs", "steps" : 
-        """
-Heat oven to 220C/200C Fan/gas 7. Cut a slit halfway in each chicken breast, then put each one between two pieces of baking parchment and bash with a rolling pin to flatten slightly./n
-Cut your sheet of pastry in half widthways and put both halves on a baking tray. Sit a chicken breast on top of each half, then cram the cavities with the Boursin (don’t worry if a bit of cheese oozes out). Season, scatter over the lemon zest then fold the edges of the pastry in to the centre and pinch shut. Flip each parcel over so the seal is on the bottom. Brush with a little oil, then bake for 30 mins until deep golden./n
-About 10 mins before your parcels are ready, steam or boil the greens until tender. Toss in the oil and season. Serve alongside your chicken parcels with lemon wedges for squeezing over.
-        """, "ingredients" : {"Chicken":["2 Breasts of",False], "Olive Oil":["1 tbsp",False], "Green Bean":["200g",True], "Garlic":["150g",False], "Puff Pastry":["320g",False], "Lemon" : ["1/2 of a",False]}, "category" : ["Mains", "Easy Meals", "Under 30'"], "prep_time" : 10, "cook_time" : 20, "servings" : 2,
-         "difficulty" : "easy", "pub_date" : datetime.date(2021,3,20), "stars" : 60, "filename" : "garlic-chicken-parcels.jpg"}
+        """Heat oven to 220C/200C Fan/gas 7. Cut a slit halfway in each chicken breast, then put each one between two pieces of baking parchment and bash with a rolling pin to flatten slightly.
+Cut your sheet of pastry in half widthways and put both halves on a baking tray. Sit a chicken breast on top of each half, then cram the cavities with the Boursin. Season, scatter over the lemon zest then fold the edges of the pastry in to the centre and pinch shut. Flip each parcel over so the seal is on the bottom. Brush with a little oil, then bake for 30 mins until deep golden.
+About 10 mins before your parcels are ready, steam or boil the greens until tender. Toss in the oil and season. Serve alongside your chicken parcels with lemon wedges for squeezing over.""", "ingredients" : {"Chicken":["2 Breasts of",False], "Olive Oil":["1 tbsp",False], "Green Bean":["200g",True], "Garlic":["150g",False], "Puff Pastry":["320g",False], "Lemon" : ["1/2 of a",False]}, "category" : ["Mains", "Easy Meals", "Under 30'"], "prep_time" : 10, "cook_time" : 20, "servings" : 2,
+         "difficulty" : "easy", "pub_date" : datetime.date(2021,3,20), "stars" : 60, "filename" : "garlic-chicken-parcels.jpg"},
 
-         
+         {"name":"Pasta", "author" : "benking", "steps" : 
+        """Heat a pot of saltwater big enough to accommodate your cooked pasta plus an inch or two of pasta water (for a pound of pasta, you'll want four quarts).
+When it's boiling (you'll see big bubbles in the water and steam rising from the pot), add your pasta
+After it comes to a boil again, set a timer to the minutes specified on the pasta box, adjusting the flame downward if it begins boiling over, which sometimes happens.
+When the timer goes off, drain and rinse the pasta in cool water to stop the cooking process.""", "ingredients" : {"Pasta":["500g",False], "Water":["1 Litre",False]}, "category" : ["Mains", "Easy Meals", "Under 30'", "Vegan"], "prep_time" : 2, "cook_time" : 10, "servings" : 1,
+         "difficulty" : "easy", "pub_date" : datetime.date(2022,3,14), "stars" : 500, "filename" : "pasta.jpg"},
+
+         {"name":"Omelet", "author" : "sallywalker", "steps" : 
+        """Whisk together the eggs and heavy cream in a small bowl.
+Heat a non-stick pan over medium heat. Melt the butter on the pan.
+Pour in the egg mixture and cook the eggs for 1 to 2 minutes—or until the eggs are halfway cooked.
+Place your desired fillings in the middle of the omelet.
+Using a spatula, fold the omelet in half. Cook for another 30 seconds, then slide the omelet to a plate. Serve immediately.""", "ingredients" : {"Egg":["2",True], "Cream":["1 tbsp",False], "Butter" : ["1/2 tbsp", False]}, "category" : ["Mains", "Easy Meals", "Under 30'", "Breakfast"], "prep_time" : 5, "cook_time" : 5, "servings" : 1,
+         "difficulty" : "medium", "pub_date" : datetime.date(2022,3,14), "stars" : 22, "filename" : "omelet.jpg"},
+
+         {"name":"Grilled Chicken and Avocado Salad", "author" : "johndoe", "steps" : 
+        """Combine the chicken, arugula, cranberries, avocado, goat cheese, walnuts, vinaigrette, salt, and pepper in a large bowl, using your hands or 2 forks to fully incorporate the dressing.""", "ingredients" : {"Chicken":["12 Oz",False], "Arugula":["12 Cups",False], "Cranberry" : ["1/4 cup", True], "Avocado":["1",False], "Goat Cheese":["1/4",False], "Walnut":["1/4 Cup",True], "Honey Mustard Vinaigrette":["1/4 Cup",False]}, "category" : ["Mains", "Easy Meals", "Vegan", "Vegetarian", "Lunch"], "prep_time" : 5, "cook_time" : 0, "servings" : 4,
+         "difficulty" : "easy", "pub_date" : datetime.date(2022,2,14), "stars" : 50, "filename" : "grilled-chicken-and-avocado-salad.jpg"},
+
     ]
 
 
